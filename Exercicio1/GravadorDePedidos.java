d40d759f1cfcecddf0125a28782ed6a73055fa3e
jailsonCunha <jailson.cunha@dce.ufpb.br>
2012-11-19 16:30:35 -0200

    Update Exercicio1/GravadorDePedidos.java

--- Exercicio1/GravadorDePedidos.java
+++ Exercicio1/GravadorDePedidos.java
@@ -1,27 +1,11 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 public class GravadorDePedidos {
 	
-	public List<Pedido> lePedidos() throws IOException, ClassNotFoundException{
-		List<Pedido> pedidos = new ArrayList<Pedido>();
-		
-		ObjectInputStream ler = new ObjectInputStream(new FileInputStream("ListaDePedidos.txt"));
-		pedidos = (List<Pedido>) ler.readObject();
-		ler.close(); 
-		
-		return pedidos;		
+	public List<Pedido> lePedidos() throws IOException{
+		return null;		
 	}
 	
 	public void gravaPedidos(List<Pedido> pedidos) throws IOException{
-		ObjectOutputStream escrever = new ObjectOutputStream(new FileOutputStream("ListaDePedidos.txt"));
-		escrever.writeObject(pedidos);
-		escrever.close();
+		
 	}
 
 }
